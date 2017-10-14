84b6ae460bcc6816122789b0e3a7334c8b88289f
mstaro <marcel.starosielsky@weather.com>
2017-10-14 19:59:29 -0400

    Fixed issue with jedis cluster discovery

--- docker-data/docker-entrypoint.sh
+++ docker-data/docker-entrypoint.sh
@@ -21,7 +21,8 @@ if [ "$1" = 'redis-cluster' ]; then
     supervisord -c /etc/supervisor/supervisord.conf
     sleep 3
 
-    IP=`ifconfig | grep "inet addr:17" | cut -f2 -d ":" | cut -f1 -d " "`
+    # required so that jedis can connect to cluster during discovery
+    IP=127.0.0.1 #`ifconfig | grep "inet addr:17" | cut -f2 -d ":" | cut -f1 -d " "`
     echo "yes" | ruby /redis/src/redis-trib.rb create --replicas 1 ${IP}:7000 ${IP}:7001 ${IP}:7002 ${IP}:7003 ${IP}:7004 ${IP}:7005
     tail -f /var/log/supervisor/redis*.log
 else
